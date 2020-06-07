3a65b6d8b572a07a03092d8158e30f6bb4e7f518
Tiffany Crosby <tiffany.crosby65@gmail.com>
2020-06-07 11:51:00 -0400

    modified server to add a GET request

--- api/server.js
+++ api/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const helmet = require('helmet');
+const db = require('../data/db')
 
 const apiRouter = require('./api-router.js');
 
@@ -8,5 +9,16 @@ const server = express();
 server.use(helmet());
 
 server.use('/api', apiRouter);
+
+server.get('/', async (req, res)=> {
+    try{
+        const shouts = await db('shouts');
+        res.status(200).json(`<h2>The message of the day is: ${process.env.MOTD}, ${shouts}</h2>`)
+    }
+    catch(error){
+        console.log(error);
+        res.status(500).json(`errorMessage: Can not retrieve the shouts`)
+    }
+})
 
 module.exports = server;
